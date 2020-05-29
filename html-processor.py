a66693e11c4deea258ec5e8933175e4c83ccf9cb
Afentoulis Konstantinos <kostasgoume@hotmail.com>
2020-05-29 23:30:08 +0300

    Update html-processor.py

--- html-processor.py
+++ html-processor.py
@@ -11,21 +11,20 @@ rexp1 = re.compile(r'<title>(.+?)</title>')
 
 #vima 2 apalifi sxolion
 rexp2 = re.compile(r'<!.*?>',re.DOTALL)
-  text1 = rexp2.sub('', text)
+text1 = rexp2.sub('', text)
 
 #vima 3 apalifi script kai style
 rexp3 = re.compile(r'(<script.+?</script>)|(<style.+?</style>)')
-  text2 = rexp3.sub('',text1)
-
+text2 = rexp3.sub('',text1)
 
 #vima 4 eksagogi kai ektiposi href apo <a>
 rexp4 = re.compile(r"<a(.+?)</a>',re.DOTALL)
-  for m in rexp4.finditer(text2):
+ for m in rexp4.finditer(text2):
     print(m.group(0))
 
 #vima 5 apalifi tags
 rexp5 = re.compile(r'<.+?>')
-  text3 = rexp5.sub('', text2)
+text3 = rexp5.sub('', text2)
 
 def func(m): #dilosi sinartisis pou metatrepei ola ta html entities simfona me ton pinaka
 
@@ -40,11 +39,11 @@ def func(m): #dilosi sinartisis pou metatrepei ola ta html entities simfona me t
                    
 # vima 6 metatropi ton html entities me xrisi tis sunartisis func              
 rexp6 = re.compile(r'&(amp|gt|lt|nbsp);')
-  text4 = rexp6.sub(func,text3) 
+text4 = rexp6.sub(func,text3) 
                    
 # vima 7 metatropi sinexomenon xaraktiron whitespace se ena keno                   
 rexp7 = re.compile(r'\s+') 
-  text5 = rexp7.sub(' ',text4)
+text5 = rexp7.sub(' ',text4)
                    
 # vima 8 ektiposi telikou keimenou meta apo tis epeksergasies                   
 print(text5)                   
